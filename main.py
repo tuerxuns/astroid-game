7b24e36bc7a69290b72243cee0fb834894ac2e6a
tuerxuns <tuerxuns@gmail.com>
2025-12-08 03:49:32 -0800

    Add player class with movement and rotation controls
    Add constants for player properties
    
    Add base CircleShape class for game objects
    
    Initialize game loop with delta time tracking

--- main.py
+++ main.py
@@ -2,9 +2,21 @@ import pygame
 
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 pygame.init()
+clock_object = pygame.time.Clock()
+dt = 0
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+updatable = pygame.sprite.Group()
+drawable = pygame.sprite.Group()
+
+Player.containers = (updatable, drawable)
+player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
+
+
 
 while True:
     log_state()
@@ -12,11 +24,17 @@ while True:
         if event.type == pygame.QUIT:
             continue
     screen.fill("black")
+    updatable.update(dt)
+    for sp in drawable:
+        sp.draw(screen)
     pygame.display.flip()
+    dt = clock_object.tick(60)/1000
+
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+    
 
 
 
